7d834aa2dc60d358ef123d233ebad0bc4e9f624d
LordCracken <vladislav070201@gmail.com>
2023-03-23 18:29:21 +0300

    solve dominant direction exercise

---
+++ exercises/chapter-5-hocs/dominantDirection.js
@@ -0,0 +1,15 @@
+// Для работы требуется окружение из песочницы по адресу
+// https://eloquentjavascript.net/code/#5.4
+const dominantDirection = text => {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : null;
+  }).filter(({ name }) => name !== null);
+
+  if (scripts.length === 0) return 'No scripts found';
+
+  return scripts.reduce((a, b) => a.count > b.count ? a : b).name;
+};
+
+console.log(dominantDirection('Hello!'));
+console.log(dominantDirection('Hey, مساء الخير'));
